22ecb8263af4669ee4f580e25576aae941305128
bboysilver <126320947+bboysilver@users.noreply.github.com>
2026-07-23 19:19:21 +0900

    Remove advertising ID and prepare closed testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
     applicationId = "com.bboysilver.lifelink"
     minSdk = 24
     targetSdk = 36
-    versionCode = 22
-    versionName = "2.2.1"
+    versionCode = 23
+    versionName = "2.2.2"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
